b50eb3c2995a5ba1ff296c189673084973ed5a1f
Monica Lent <lent.monica@gmail.com>
2015-02-10 09:13:39 +0100

    Added root alias for tmux to use.

--- .bash_profile
+++ .bash_profile
@@ -1,3 +1,4 @@
 alias ls="ls -laF"
 alias sp="cd /opt/ze-dashboard/app/scripts"
+alias spr="cd /opt/ze-dashboard"
 alias spt=". ~/tmux/sp-session.sh"
--- tmux/sp-session.sh
+++ tmux/sp-session.sh
@@ -8,8 +8,8 @@ if [ "$?" -eq 1 ]; then
   tmux new-window -t sp:9 -n node
 
   tmux send-keys -t sp:0 'sp; clear' C-m
-  tmux send-keys -t sp:8 'sp; grunt sass-watch' C-m
-  tmux send-keys -t sp:9 'sp; cd ../web-backend; node app' C-m
+  tmux send-keys -t sp:8 'spr; grunt sass-watch' C-m
+  tmux send-keys -t sp:9 'spr; cd ../web-backend; node app' C-m
 
   tmux select-window -t sp:0
 fi
