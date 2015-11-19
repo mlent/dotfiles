403e88e5805a223b124d69440f14da8bf6274cc0
Monica Lent <lent.monica@gmail.com>
2015-11-19 09:37:13 +0100

    Update tmux settings.

--- tmux/sp-session.sh
+++ tmux/sp-session.sh
@@ -8,7 +8,7 @@ if [ "$?" -eq 1 ]; then
   tmux new-window -t sp:9 -n node
 
   tmux send-keys -t sp:0 'sp; clear' C-m
-  tmux send-keys -t sp:8 'spr; gulp dev' C-m
+  tmux send-keys -t sp:8 'spr; gulp dev:build' C-m
   tmux send-keys -t sp:9 'spr; cd ../web-backend; node app' C-m
 
   tmux select-window -t sp:0
